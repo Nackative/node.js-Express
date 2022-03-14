4d3e1a839423c377e24be96189d9490992be3eed
NS-Family <bossvglk@gmail.com>
2022-03-14 16:39:34 +0700

    update index.ejs
    v.1

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require("path");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname ,"/public/")));
+
+app.set("views","./src/views");
+app.set("view engine", "ejs")
 
 app.get("/", (req,res) =>{
 
-    res.send('Hello brontodev');
+    res.render('index',{username: 'Snack', customers: ["Coffee","Mango","NaTo"]});
 
 })
 
-app.listen(port, ()=>{
-    debug("Listerning on port" + chalk.green(" : " +port));
+app.listen(PORT, ()=>{
+    debug("Listerning on port" + chalk.green(" : " +PORT));
 })
